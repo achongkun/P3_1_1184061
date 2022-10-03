be62cd40fd873f1d33c90c8eb60a837cbdc0ccae
Ida Bagus Aditya Sueken <idabagusaditya21@gmail.com>
2022-10-03 16:31:39 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_1_1184061
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ayam;
+            Console.Write("ANAK AYAM TURUN");
+            ayam = int.Parse(Console.ReadLine());
+
+            if (ayam > 10 || ayam <= 0)
+            {
+                Console.WriteLine("tidak sesuai");
+            }
+            else
+            {
+                for (int i = ayam; i >= 1; i--)
+                {
+
+
+
+                    if (i == 1)
+                    {
+                        Console.WriteLine("lebih dari ayam 1 " + "mati satu tinggal induknya");
+                    }
+                    else
+                    {
+                        Console.WriteLine("lebih dari ayam " + i + "," + "Matilah satu tinggalah " + (i - 1));
+                    }
+
+                }
+            }
+        }
+    }
+}
